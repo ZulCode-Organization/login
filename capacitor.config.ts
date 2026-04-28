a94029b1f80fa2a87dafcb588b8729701d20aa4c
Thales Gregorio <137453849+thales-gsd@users.noreply.github.com>
2026-04-28 16:39:51 -0300

    feat: criando porte mobile

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zulcode',
+  appName: 'ZulCode',
+  webDir: 'out',
+};
+
+export default config;
